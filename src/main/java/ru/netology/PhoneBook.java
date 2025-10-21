aec8f4d5af2f506d2a800ec064f4b8407c20c241
SashaDch <sashadychko@mail.ru>
2025-10-21 19:35:17 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,13 @@
 package ru.netology;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PhoneBook {
+    Set<String> names = new HashSet<>();
+
     int add(String name, String phoneNumber) {
-        return 0;
+        names.add(name);
+        return names.size();
     }
 }
