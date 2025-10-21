1c291d9505f759890ce3a4402bb79ef041aec850
SashaDch <sashadychko@mail.ru>
2025-10-21 19:54:57 +0300

    findByNumber implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,17 +1,23 @@
 package ru.netology;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PhoneBook {
-    Set<String> names = new HashSet<>();
+    Map<String, String> names = new HashMap<>();
+    Map<String, String> numbers = new HashMap<>();
 
     int add(String name, String phoneNumber) {
-        names.add(name);
+        if (name == null || phoneNumber == null) {
+            throw new NullPointerException("Name and number must be not-null!");
+        }
+        if (names.putIfAbsent(name, phoneNumber) == null) {
+            numbers.put(phoneNumber, name);
+        }
         return names.size();
     }
 
     String findByNumber(String phoneNumber) {
-        return null;
+        return numbers.get(phoneNumber);
     }
 }
